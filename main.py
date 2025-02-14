95bb412fd48e5857b35604c9d141ded972777058
RenzSamsonSchleiden <renzsamsung12@gmail.com>
2025-02-14 14:34:13 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import turtle as t 
+
+t.speed(30)
+
+pattern=1
+t.bgcolor("orange")
+
+for i in range (100):
+    for color in ["black","blue"]:
+        t.color(color)
+        t.forward(pattern)
+        t.right(90)
+        t.left(45)
+        pattern+=1
+        #This is an octagon
+        #Black and Blue are it's main colors
+        #It is a pattern
+        #It follows an arrow
+        #It has a background color of orange
